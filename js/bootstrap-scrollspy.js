e2277ced06692778e1d9fbf324de19cb3727fb69
Bart Gloudemans <bart@supportthesystem.org>
2013-09-01 11:30:41 +0200

    specific scroll target, enables to separate target from source

--- js/bootstrap-scrollspy.js
+++ js/bootstrap-scrollspy.js
@@ -34,7 +34,7 @@
     this.$scrollElement = $element.on('scroll.scroll-spy.data-api', process)
     this.selector = (this.options.target
       || ((href = $(element).attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '')) //strip for ie7
-      || '') + ' .nav li > a'
+      || '') + ' .nav li > a.scroll-spy-link'
     this.$body = $('body')
     this.refresh()
     this.process()
